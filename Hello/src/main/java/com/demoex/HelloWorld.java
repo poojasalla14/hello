433173458e126f37036d758421a9c9ee69e4d758
Pushpanjali <poojasalla14@gmail.com>
2021-01-29 15:37:55 +0530

    hlooo

---
+++ Hello/src/main/java/com/demoex/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.demoex;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello wolrd");
+		System.out.println("how is ur trainning goin on?");
+
+	}
+
+}
